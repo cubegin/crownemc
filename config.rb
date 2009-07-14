741b91622c513b17d160d060325f8272930c09b6
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-07-14 17:11:44 +0530

    Corrected images directory in config.rb

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 project_type = :stand_alone
 css_dir = "stylesheets"
 sass_dir = "src"
-images_dir = "statics"
+images_dir = "statics/images"
 output_style = :compressed
 # To enable relative image paths using the images_url() function:
 # http_images_path = :relative
